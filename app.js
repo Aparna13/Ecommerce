cb3f44daf4201db75f6edfc6dd3e0092775ec801
Aparna <aparna.sainath@gmail.com>
2015-09-02 13:39:04 -0700

    product detail page

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 $( document ).ready(function() {
+  $('.productsingle').css("display","none");
   console.log("ready ");
   var counter = 0;
 
   var data = [
   {
     imgsrc:"http://i.imgur.com/KV2e9jE.jpg",
+    imgsrc1:"",
     price: "$ 39.99",
     name: "Black White Gray",
     shortlisted: false,
@@ -65,7 +67,7 @@ $( document ).ready(function() {
 
   data.forEach(function add (n,i) {
                  
-    $('.contentadd').append("<div class = 'col-sm-5 product'><a href='#'><img src= '" + n.imgsrc + " ' class = 'img-thumb'></a><div class='ontop' data-index='" + i + "'><span class ='btn btn-primary btn-box'>Shortlist </span></div><p><p class ='producttitle'> " + n.name + "<span class ='fontcolor text-right'>$ 39.99</span></p><p class ='size'>s m l</p></div></div>");
+    $('.contentadd').append("<div class = 'col-sm-5 product'><a href='#' class='productdetail'><img src= '" + n.imgsrc + " ' class = 'img-thumb'></a><div class='ontop' data-index='" + i + "'><span class ='btn btn-primary btn-box'>Shortlist </span></div><p><p class ='producttitle'> " + n.name + "<span class ='fontcolor text-right'>$ 39.99</span></p><p class ='size'>s m l</p></div></div>");
     
     });
 
@@ -80,27 +82,55 @@ $( document ).ready(function() {
     // console.log(data[$(this).data('index')].shortlisted));
  
     counter++;
-    $('.shortlist').text(counter);
+    $('.shortlisted').text(counter);
 
 
   });
 
 
   $('#shortlist').on("click", function (e) {
+    e.preventDefault();
     $(this).css("background-color", "#333333");
     $(this).css("color", "white");
-    $('#outline').attr("class", "btn btn-default tog smtext");
+    $('#all').attr("class", "btn btn-default tog smtext outline");
+    $('.shortlistadd').removeClass("hidden");
+     $('.shortlistadd').empty();  
 
     data.forEach(function addnew (n) {
       console.log(n.shortlisted);
-     if (n.shortlisted == true){
+     if (n.shortlisted === true){
       console.log("this is true");
-      $('.contentadd').remove();            
+      $('.contentadd').css("display","none");
+              
       $('.shortlistadd').append("<div class = 'col-sm-5 product'><a href='#'><img src= '" + n.imgsrc + " ' class = 'img-thumb'></a><p class ='producttitle'> " + n.name + "<span class ='fontcolor text-right'>$ 39.99</span></p><p class ='size'>s m l</p></div></div>");
      }
    
     });
+  });
 
+  $('#all').on("click", function (e) {
+    e.preventDefault();
+    $(this).css("background-color", "#333333");
+    $(this).css("color", "white");
+    $('#shortlist').css("background-color", "white");
+    $('#shortlist').css("color", "#333333");
+    $('.shortlistadd').addClass("hidden");
+    $('.contentadd').css("display","block");
+
+  });
+
+  $('.img-thumb').on("click", function (e) {
+    e.preventDefault();
+    // var newsrc = data[$(this).data('index')].imgsrc ;
+    var newsrc = data[$(this).parent().next('.ontop').data('index')].imgsrc;
+    // var newsrc = data[$(this).next('.ontop').data('index')].imgsrc;
+    $('.contentadd').css("display","none");
+    $('.left').css("display","none");
+    $('.right').css("display","none");
+    $('.filter').css("display","none");
+
+    $('.productsingle').css("display","block");
+    $('.largeimg').attr("src",newsrc);
 
   });
 
